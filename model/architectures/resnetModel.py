0f511b99007d6d3426bf8911820bd0fe413ab347
st148385 <st148385@stud.uni-suttgart.de>
2020-05-14 11:28:14 +0200

    linux

--- model/architectures/resnetModel.py
+++ model/architectures/resnetModel.py
@@ -3,18 +3,17 @@ import gin
 
 
 
-@gin.configurable('simpleModel')
-def Architechture(input_size=32, n_filters_in=16, kernel_size=3, neurons=128):
+@gin.configurable('resnetModel')
+def Architechture(input_size=224, n_filters_in=16, kernel_size=3, neurons=128):
 
-    rn50 = tf.keras.applications.ResNet50(include_top=False, weights=None, input_tensor=None, input_shape=None,
-                                          pooling='avg')
+    rn50 = tf.keras.applications.ResNet50(include_top=False, weights=None, input_tensor=None, input_shape=None)
     rn50.training = True
 
     # Layers
     # f(•)
-    inputs = tf.keras.Input((224, 224, 3))
+    inputs = tf.keras.Input((input_size, input_size, 3))
     h_a = rn50(inputs)
-    #h_a = tf.keras.layers.GlobalAveragePooling2D()(h_a)
+    h_a = tf.keras.layers.GlobalAveragePooling2D()(h_a)
     # g(•)
     z_a = tf.keras.layers.Dense(512)(h_a)
     z_a = tf.keras.layers.Activation("relu")(z_a)
